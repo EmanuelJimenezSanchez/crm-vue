3cef055c668a9fc8c281be8a9f7110e43fca0840
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-01-30 14:11:15 -0600

    New client form with FormKit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { FormKit } from '@formkit/vue';
   import RouterLink from '../components/UI/RouterLink.vue'
   import Heading from '../components/UI/Heading.vue'
 
@@ -7,6 +8,10 @@
       type: String
     }
   })
+
+  const handleSubmit = (data) => {
+    console.log(data)
+  }
 </script>
 
 <template>
@@ -18,6 +23,73 @@
     </div>
 
     <Heading>{{ titulo }}</Heading>
+
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit
+          type="form"
+          submit-label="Agregar Cliente"
+          incomplete-message="Faltan campos por llenar"
+          @submit="handleSubmit"
+        >
+          <FormKit 
+            type="text"
+            label="Nombre"
+            name="nombre"
+            placeholder="Nombre del cliente"
+            validation="required"
+            :validation-messages=" {required: 'El nombre del cliente es obligatorio'}"
+          />
+
+          <FormKit 
+            type="text"
+            label="Apellido"
+            name="apellido"
+            placeholder="Apellido del cliente"
+            validation="required"
+            :validation-messages=" {required: 'El apellido del cliente es obligatorio'}"
+          />
+
+          <FormKit 
+            type="text"
+            label="Email"
+            name="email"
+            placeholder="Email de cliente"
+            validation="required|email"
+            :validation-messages=" {required: 'El email del cliente es obligatorio', email: 'Coloca un email válido'}"
+          />
+
+          <FormKit 
+            type="text"
+            label="Teléfono"
+            name="telefono"
+            placeholder="XXX-XXX-XXXX"
+            validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages=" {matches: 'El formato no es válido'}"
+          />
+
+          <FormKit 
+            type="text"
+            label="Empresa"
+            name="empresa"
+            placeholder="Empresa del cliente"
+          />
+
+          <FormKit 
+            type="text"
+            label="Puesto"
+            name="puesto"
+            placeholder="Puesto del cliente"
+          />
+        </FormKit>
+
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+  .formkit-wrapper {
+    max-width: 100%;
+  }
+</style>
